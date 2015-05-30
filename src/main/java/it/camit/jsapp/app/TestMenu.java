2b145bc4fd09dd184c99286593c761c28c1224e7
Paolo Denti <paolo.denti@gmail.com>
2015-05-30 19:50:16 +0200

    .

--- src/main/java/it/camit/jsapp/app/TestMenu.java
+++ src/main/java/it/camit/jsapp/app/TestMenu.java
@@ -19,12 +19,15 @@
 
 package it.camit.jsapp.app;
 
+import it.camit.jsapp.core.util.command.base.SappConnection;
+
 import java.util.Scanner;
 
 public class TestMenu {
 
-	private String hostname = null;
+	private String hostName = null;
 	private int portNumber = 0;
+	private SappConnection sappConnection = null;
 
 	public static void main(String[] args) {
 		new TestMenu().run(args);
@@ -47,22 +50,36 @@ public class TestMenu {
 	private void getDeviceAddress(Scanner input) {
 
 		while (true) {
-			System.out.print("Enter device address: ");
-			hostname = input.nextLine();
-			if (hostname.length() > 0) {
-				break;
+			while (true) {
+				System.out.print("Enter device address: ");
+				hostName = input.nextLine();
+				if (hostName.length() > 0) {
+					break;
+				}
 			}
-		}
 
-		while (true) {
+			while (true) {
 
-			System.out.print("Enter device port: ");
-			String port = input.nextLine();
+				System.out.print("Enter device port: ");
+				String port = input.nextLine();
+				try {
+					portNumber = Integer.parseInt(port);
+					break;
+				} catch (NumberFormatException e) {
+					System.out.println("Bad address");
+				}
+			}
+
+			if (sappConnection != null) {
+				sappConnection.closeConnection();
+			}
+
+			sappConnection = new SappConnection(hostName, portNumber);
 			try {
-				portNumber = Integer.parseInt(port);
+				sappConnection.openConnection();
 				break;
-			} catch (NumberFormatException e) {
-				System.out.println("Bad address");
+			} catch (Exception e) {
+				System.out.println("Cannot open connection to device");
 			}
 		}
 	}
@@ -70,22 +87,43 @@ public class TestMenu {
 	private void presentMenu(Scanner input) {
 
 		while (true) {
-			System.out.println("hostname: " + hostname);
-			System.out.println("portNumber: " + portNumber);
+			System.out.println("===================================");
+			if (sappConnection != null && sappConnection.isConnected()) {
+				System.out.println(String.format("Connected to %s:%d", hostName, portNumber));
+			} else {
+				System.out.println("Not connected");
+			}
 
+			System.out.println("===================================");
 			System.out.println("          JSapp test menu");
 			System.out.println("===================================");
 
 			System.out.println(" 1) Change device address & port");
+			System.out.println(" 2) Connect to device");
+			System.out.println(" 3) Disconnect from device");
+			System.out.println();
+			System.out.println("7C) execute 0x7C command (Get Virtual Status WORD)");
+			System.out.println("7D) execute 0x7D command (Set Virtual Status WORD)");
+			System.out.println("7E) execute 0x7E command (Get Virtual Status 32 WORD)");
+			System.out.println("82) execute 0x82 command (Get Last Virtual WORD)");
+			System.out.println();
 			System.out.println("99) Exit");
 			System.out.println("===================================");
 
 			String choice = input.nextLine();
 			if ("99".equals(choice)) {
+				if (sappConnection != null) {
+					sappConnection.closeConnection();
+				}
+
 				return;
 			} else if ("1".equals(choice)) {
 				getDeviceAddress(input);
 			}
 		}
+	}
+
+	private void execute7C() {
+
 	}
 }
